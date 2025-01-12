4db967e1add8e5c0663e5cbbc69f7561b2647d30
Phill-Chill <philipe2015amancio@hotmail.com>
2025-01-11 21:16:33 -0300

    Atualização 11/01

--- community/models.py
+++ community/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 class Community(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    banner = models.ImageField(upload_to='images/banners/', null=True, blank=True)
-    visibility_students = models.BooleanField(default=True)
-    visibility_teachers= models.BooleanField(default=True)
+    communityName = models.CharField(max_length=100)
+    communityDescription = models.TextField()
+    # banner = models.ImageField(upload_to='images/banners/', null=True, blank=True)
+    visibility = models.CharField(max_length = 100, default='')
+    passwordRequired = models.BooleanField(default=False)
+    password = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    ## Subcommunity
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='subcommunities', on_delete=models.SET_NULL)
+    is_subcommunity = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.parent:
+            self.is_subcommunity = True
+        else:
+            self.is_subcommunity = False
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.name
+        return self.communityName
--- community/views.py
+++ community/views.py
@@ -6,4 +6,30 @@ from rest_framework.permissions import IsAuthenticated
 class CommunityViewset(viewsets.ModelViewSet):
     queryset = Community.objects.all()
     serializer_class = CommunitySerializer
+<<<<<<< Updated upstream
     # permission_classes = [IsAuthenticated]
+=======
+    # permission_classes = [IsAuthenticated]
+
+    #Filtragem de comunidades por tipo de usuário
+    def get_queryset(self):
+        user_type = self.request.query_params.get('type', None)
+
+        if user_type == 'Professor':
+            return Community.objects.filter(visibility='Professor')
+
+        elif user_type == 'Aluno':
+            return Community.objects.filter(visibility = 'Aluno')
+        
+        else:
+            return Community.objects.all()
+
+    #Acesso só através da comunidade pai
+    @action(detail=True, methods=['get'])
+    def subcommunities(self, request, pk=None):
+        community = get_object_or_404(Community, pk=pk)
+        subcommunities = community.subcommunities.filter(is_subcommunity=True)
+        serializer = CommunitySerializer(subcommunities, many=True)
+        return Response(serializer.data)
+    
+>>>>>>> Stashed changes
