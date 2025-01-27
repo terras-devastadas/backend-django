ed471d0b72a644d61cf8df36ead417a2721073a0
Philipe <philipe2015amancio@hotmail.com>
2025-01-27 19:24:31 -0300

    Post // author-fix

--- posts/views.py
+++ posts/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 import base64
 import io
-from PIL import Image    
+from PIL import Image
+from rest_framework.response import Response
 
 class PostViewSet(viewsets.ModelViewSet):
+    permmision_classes = [IsAuthenticated]
+    
+
+    
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(author_username=self.request.user)
 
     # def get_queryset(self):
     #     community_id = self.request.query_params.get('community', None)
