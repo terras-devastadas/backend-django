65706a1e081ff1148502655a56b741ffe5d1988e
Philipe <philipe2015amancio@hotmail.com>
2025-02-08 22:34:37 -0300

    integração-fix (profile,edit)

--- customUser/models.py
+++ customUser/models.py
@@ -6,3 +6,7 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, default='')
     photo_profile = models.ImageField(upload_to='images/profile_pics/', blank=True, null=True)
     matricula = models.CharField(max_length=8, blank=True)
+    course = models.CharField(max_length=100, blank=True, default='')
+    semester = models.CharField(max_length=100, blank=True, default='')
+    subject = models.CharField(max_length=100, blank=True, default='')
+    food = models.CharField(max_length=100, blank=True, default='')
